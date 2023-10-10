17bcf04a10530505ca6cfaaee1e4dbffdcd14eb8
Mickman7 <101274574+Mickman7@users.noreply.github.com>
2023-10-10 15:17:52 +0100

    comment out

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let navLinks = document.getElementById("nav-links");
 let links = document.querySelectorAll(".links");
 
 hamburger.addEventListener("click", () => {
-        navLinks.classList.toggle("hide");
+        //navLinks.classList.toggle("hide");
         hamburger.classList.toggle("lines-rotate");
 });
 
